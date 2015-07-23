f3298bb5e10ac93a48abf90c375744d9952806ff
tobias <uk001333@student.uni-kassel.de>
2015-07-23 07:31:55 +0200

    - draw volt and ampere

--- frontend/app/scripts/directives/lineChart.js
+++ frontend/app/scripts/directives/lineChart.js
@@ -5,76 +5,122 @@ angular.module('frontendApp')
 
     $log.info('lineChart start');
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 50};
+    var margin = {top: 20, right: 50, bottom: 30, left: 50};
     var width = 960 - margin.left - margin.right;
     var height = 500 - margin.top - margin.bottom;
     var svg;
+
+    var scaleTime, scaleVolt, scaleAmpere;
+    var axisTime, axisVolt, axisAmpere;
+    var lineVolt, lineAmpere;
+
+    function drawDiagram(element) {
+      var rawSvg=element.find('svg');
+      svg = d3.select(rawSvg[0]);
+
+      svg = svg.attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      scaleTime = d3.scale.linear()
+        .range([0, width]);
+
+      scaleVolt = d3.scale.linear()
+        .range([height, 0]);
+
+      scaleAmpere = d3.scale.linear()
+        .range([height, 0]);
+
+      axisTime = d3.svg.axis()
+        .scale(scaleTime)
+        .orient('bottom');
+
+      axisVolt = d3.svg.axis()
+        .scale(scaleVolt)
+        .orient('left');
+
+      axisAmpere = d3.svg.axis()
+        .scale(scaleAmpere)
+        .orient('right');
+
+      lineVolt = d3.svg.line()
+        .x(function(d) { return scaleTime(d.time); })
+        .y(function(d) { return scaleVolt(d.volt); });
+
+      lineAmpere = d3.svg.line()
+        .x(function(d) { return scaleTime(d.time); })
+        .y(function(d) { return scaleAmpere(d.ampere); });
+
+      svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(axisTime);
+
+      svg.append('g')
+        .attr('class', 'y axis')
+        .call(axisVolt)
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '.71em')
+        .style('text-anchor', 'end')
+        .text('Spannung (V)');
+
+      svg.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate('+width+',0)')
+        .call(axisAmpere)
+        .append('text')
+        .attr('transform', 'rotate(90)')
+        .attr('y', 6)
+        .attr('dy', '.71em')
+        .text('Strom (A)');
+    }
 
     return {
 
       controller: function($log, $element, $scope) {
         $log.info('Start', $element, d3, margin, width, height, $scope.evaluation);
 
-        var rawSvg=$element.find('svg');
-        svg = d3.select(rawSvg[0]);
+        drawDiagram($element);
 
-        svg = svg.attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
-          .append('g')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-        var x = d3.scale.linear()
-          .range([0, width]);
-
-        var y = d3.scale.linear()
-          .range([height, 0]);
-
-        var xAxis = d3.svg.axis()
-          .scale(x)
-          .orient('bottom');
-
-        var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient('left');
-
-        var line = d3.svg.line()
-          .x(function(d) { return x(d.time); })
-          .y(function(d) { return y(d.volt); });
-
-        x.domain([
+        scaleTime.domain([
           _.min($scope.evaluation.data, 'time').time,
           _.max($scope.evaluation.data, 'time').time
         ]);
 
-        y.domain([
+        scaleVolt.domain([
           _.min($scope.evaluation.data, 'volt').volt,
           _.max($scope.evaluation.data, 'volt').volt
         ]);
 
-        svg.append('g')
-          .attr('class', 'x axis')
-          .attr('transform', 'translate(0,' + height + ')')
-          .call(xAxis);
-
-        svg.append('g')
-          .attr('class', 'y axis')
-          .call(yAxis)
-          .append('text')
-          .attr('transform', 'rotate(-90)')
-          .attr('y', 6)
-          .attr('dy', '.71em')
-          .style('text-anchor', 'end')
-          .text('Temperature (ºF)');
+        scaleAmpere.domain([
+          _.min($scope.evaluation.data, 'ampere').ampere,
+          _.max($scope.evaluation.data, 'ampere').ampere
+        ]);
 
         svg.append('svg:path')
           .attr({
-              d: line($scope.evaluation.data),
+              d: lineVolt($scope.evaluation.data),
               'stroke': 'blue',
               'stroke-width': 1.5,
               'opacity': 0.9,
               'fill': 'none',
               'class': 'path'
             });
+
+        svg.append('svg:path')
+          .attr({
+            d: lineAmpere($scope.evaluation.data),
+            'stroke': 'red',
+            'stroke-width': 1.5,
+            'opacity': 0.9,
+            'fill': 'none',
+            'class': 'path'
+          });
+
       },
 
       template: '<svg width="0" height="0"></svg>',
